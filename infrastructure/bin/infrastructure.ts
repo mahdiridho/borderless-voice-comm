7a12b74a1f05c7034c92b02f33f7764254ada4f2
Mahdi Ridho <mahdiridho@Mahdis-Laptop.local>
2025-10-12 20:43:17 +0700

    Fixed path

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { BorderlessVoiceCommStack } from './lib/borderless-voice-comm-stack';
+import { BorderlessVoiceCommStack } from '../lib/borderless-voice-comm-stack';
 
 const app = new cdk.App();
 
